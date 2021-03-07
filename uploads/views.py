c0609e8ef77940a123e99a6fef005fce59de0092
Jun Qing Lim <itsjunqing@gmail.com>
2021-03-07 17:31:10 +0800

    add api for file upload

---
+++ uploads/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import UserUploadForm
+from .models import UserUpload
+# Create your views here.
+
+from django.http import HttpResponse, JsonResponse
+from model.classifier import *
+
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+
+
+@csrf_exempt 
+def upload_files(request):
+	print("Uploading files...")
+	if request.method == 'POST':
+		form = UserUploadForm(request.POST, request.FILES)
+		files = request.FILES.getlist('file')
+		upload_results = []
+		if form.is_valid():
+			for f in files:
+				text = f.read().decode('utf-8')
+				result, percentages = run_model(text)
+				user_result = {
+					"result": result,
+					"percentages": percentages
+				}
+				upload_results.append(user_result)
+
+			return JsonResponse(upload_results, safe=False)
+
+	return Response("Unable to upload files", status=status.HTTP_403_FORBIDDEN)
+
