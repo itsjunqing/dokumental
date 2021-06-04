5976087a84936421565ef69ad840c8064b7e270c
Jun Qing Lim <itsjunqing@gmail.com>
2021-06-04 09:39:03 +0800

    Add model download

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import requests, zipfile
+from io import BytesIO
 
 def main():
     """Run administrative tasks."""
@@ -18,5 +19,19 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+def initMain():
+    if os.path.exists('model'):
+        return False
+
+    url = "https://www.dropbox.com/sh/l534p3v9u2vsghx/AABLtm7nd3ZRCiqyCKyjjbrza?dl=1"
+    print('Downloading model from Dropbox...')
+    req = requests.get(url)
+    print('Model download completed')
+
+    with zipfile.ZipFile(BytesIO(req.content)) as zip_file:
+        zip_file.extractall('model') # extract into the `model/` path
+    return True
+
 if __name__ == '__main__':
+    initMain()
     main()
